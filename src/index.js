674dc9784fdc03d469a64eb7c704fd09a72cebb0
Cedric Louyot <cedric.louyot@gmail.com>
2023-01-20 11:45:56 +0100

    Board render method uses 2 loops

--- src/index.js
+++ src/index.js
@@ -24,21 +24,11 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[...Array(3).keys()].map(row =>
+          <div className="board-row">
+            {[...Array(3).keys()].map(col => this.renderSquare(3 * row + col))}
+          </div>
+        )}
       </div>
     );
   }
